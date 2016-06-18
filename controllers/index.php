8feb04bdb58b3f38a6ea4b5b96b8b9b4ed54e776
osmansmith <osman_ahumada@hotmail.com>
2016-06-18 05:10:13 -0400

    limpiando la casa

--- controllers/index.php
+++ controllers/index.php
@@ -4,39 +4,28 @@
       # la funcion constructora hereda de controller
       function __construct()
       {
-          parent::__construct();
+           parent::__construct();
       }            
       # metodo index
       function index()
       {
           # el metodo render admite un parametro que es la pagina de la carpeta views sin el .php
-          $this->view->render('index');
+           $this->view->render('index');
       }       
+      
       function inicio()
-      {           
-         
-          $this->view->render('inicio');
-   
+      {                    
+           $this->view->render('inicio');
       }
+      
       function otro()
       {
-        $this->view->render('por_php');   
+           $this->view->render('por_php');   
       }
+      
       function guardar(){
            $this->view->render('datos');
-      }
-      public function licitacion()
-      {    
-          # Array Asociativo
-          $datos = [   
-              'nombre' => $_POST['nombre'],
-              'externo' => $_POST['externo'],
-              'cierre' => $_POST['cierre'],
-              'estado' => $_POST['estado']
-              
-          ];                                             
-              $this->model->registrar($datos);                                      
-      }    
+      }     
      
       
       
--- views/user/empaque.php
+++
@@ -1,2 +0,0 @@
-<div id="capa2" style=" min-height: 450px; padding:1% 1% 1% 1%;" class="twelve wide column">
-<div id="contenido"></div></div>
